e6ff42985b7971020d00467117340d3df03df1d4
Alex Zinkner <name.nachname@gmx.at>
2017-03-07 09:09:24 +0100

    Person Class

--- src/threadwithpacher2/Person.java
+++ src/threadwithpacher2/Person.java
@@ -5,10 +5,65 @@
  */
 package threadwithpacher2;
 
+import java.util.Date;
+
 /**
  *
  * @author micha
  */
 public class Person {
+    
+    private String vn;
+    private String nn;
+    private Date gebdate;
+    private String adresse;
+    
+    
+    private enum Gender {
+        MALE, FEMALE
+    }
+
+    public Person(String vn, String nn, Date gebdate, String adresse) {
+        this.vn = vn;
+        this.nn = nn;
+        this.gebdate = gebdate;
+        this.adresse = adresse;
+    }
+
+    public String getVn() {
+        return vn;
+    }
+
+    public void setVn(String vn) {
+        this.vn = vn;
+    }
+
+    public String getNn() {
+        return nn;
+    }
+
+    public void setNn(String nn) {
+        this.nn = nn;
+    }
+
+    public Date getGebdate() {
+        return gebdate;
+    }
+
+    public void setGebdate(Date gebdate) {
+        this.gebdate = gebdate;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+    
+    
+    
+    
     
 }
