a48b79a89cc6d97d1ea90426ff9a2885d350c7b1
PACHER Michael Kurt <pacmib13@htl-kaindorf.ac.at>
2017-03-07 08:39:39 +0100

    First Class

--- src/threadwithpacher2/Model_BL.java
+++ src/threadwithpacher2/Model_BL.java
@@ -7,15 +7,8 @@ package threadwithpacher2;
 
 /**
  *
- * @author alex
+ * @author micha
  */
-public class ThreadWithPacher2 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class Model_BL {
     
 }
